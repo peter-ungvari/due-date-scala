98618523c52869a17b332270eaa54e9453574c40
Peter Ungvari <ungvari.mail@gmail.com>
2016-09-10 19:05:05 +0200

    bug on multi day overflow

--- src/main/scala/org/example/duedate/scala/DueDateCalculator.scala
+++ src/main/scala/org/example/duedate/scala/DueDateCalculator.scala
@@ -1,11 +1,29 @@
 package org.example.duedate.scala
 
 import java.time._
+import java.time.temporal.ChronoUnit
 
 object DueDateCalculator {
 
   def calculateDueDate(submitDate: LocalDateTime, turnAround: Duration): LocalDateTime = {
-    submitDate.plus(turnAround)
+    val endOfDay: LocalDateTime = submitDate withHour 17
+    val secondsTilEndOfDay = submitDate.until(endOfDay, ChronoUnit.SECONDS)
+
+    if(turnAround.minusSeconds(secondsTilEndOfDay).isNegative) {
+      return submitDate plus turnAround
+    }
+
+    calculateDueDate(nextWeekDay9AM(submitDate), turnAround minusSeconds secondsTilEndOfDay)
+  }
+
+  def nextWeekDay9AM(submitDate: LocalDateTime): LocalDateTime = {
+    val nextDay: LocalDateTime = submitDate plusDays 1
+
+    if (!(Set(DayOfWeek.SUNDAY, DayOfWeek.SATURDAY) contains nextDay.getDayOfWeek)) {
+      return nextDay withHour 9
+    }
+
+    nextWeekDay9AM(nextDay)
   }
 
 }
--- src/test/scala/org/example/duedate/scala/DueDateCalculatorTests.scala
+++ src/test/scala/org/example/duedate/scala/DueDateCalculatorTests.scala
@@ -17,4 +17,22 @@ class DueDateCalculatorTests extends FunSuite {
     assert(10 === dueDate.getHour)
   }
 
+  test("dueDate is from 9AM to 5PM (2)") {
+    val submitDate = LocalDateTime.of(2016, 9, 7, 16, 1)
+    val dueDate = DueDateCalculator.calculateDueDate(submitDate, Duration.ofHours(1))
+    assert(LocalDateTime.of(2016, 9, 8, 9, 1) == dueDate)
+  }
+
+  test("2016-09-09 2PM + 7 days 4 hours = 2016-09-19 10AM") {
+    val submitDate = LocalDateTime.of(2016, 9, 9, 14, 0)
+    val dueDate = DueDateCalculator.calculateDueDate(submitDate, Duration.ofHours(24 * 7 + 4))
+    assert(LocalDateTime.of(2016, 9, 19, 10, 0) == dueDate)
+  }
+
+  test("2015-12-31 2PM + 4 hours = 2016-01-01 10AM") {
+    val submitDate = LocalDateTime.of(2015, 12, 31, 14, 0)
+    val dueDate = DueDateCalculator.calculateDueDate(submitDate, Duration.ofHours(4))
+    assert(LocalDateTime.of(2016, 1, 1, 10, 0) == dueDate)
+  }
+
 }
